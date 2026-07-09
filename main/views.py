2a20df3157530e9897c893772d1af0dfb02b4505
hol0055 <230990461+hol0055@users.noreply.github.com>
2026-07-10 00:28:12 +1000

    Backend Implementation 1
    Ran into a CSRF verification failed error (403). Think I need to take a deeper look at the CsrfViewMiddleware within the Django project settings.

--- main/views.py
+++ main/views.py
@@ -1,9 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 from django.http import HttpResponse
 from .models import UserDetails
 
-
 def index(request):
+    if request.method =="POST":
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+
+        UserDetails.objects.create(
+            email=email,
+            password=password
+        )
+
+        return redirect("index") #Reload page after saving to database
+    
     return render(request, "main/index.html")
