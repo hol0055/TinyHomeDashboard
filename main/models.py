abbab1b79b9442d3cfacb87816f794c620cb80fd
hol0055 <230990461+hol0055@users.noreply.github.com>
2026-07-09 22:43:58 +1000

    Django WORKING!
    Okay, signup/login screen has been added into the django server. The database is also working! Now need to edit the html to pass vars from the database, then I can begin work on the dashboard.

--- main/models.py
+++ main/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class UserDetails(models.Model):
+    email = models.EmailField(max_length=254)
+    password = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
+
+    class Meta:
+        db_table = "user_details"
+
+    def __str__(self):
+        return self.email
--- main/views.py
+++ main/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from .models import UserDetails
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return render(request, "main/index.html")
