bad44b0abb6c814064a3e4be79b910955175032d
hol0055 <230990461+hol0055@users.noreply.github.com>
2026-07-15 21:28:46 +1000

    Backend Implementation 2
    I created an error banner that pops up when the user does not input a required value on the login/signup page, or when an inputted value does not pass its validation tests.
    
    Whilst originally outside the scope of my project, I decided this was a required feature to improve user experience.
    
    Currently struggling with pulling data from the database for the login functionality, but signing up works perfectly, which takes the user's details and creates a new field in the database.

--- main/urls.py
+++ main/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("", views.index, name=""),
+    path("login-endpoint", views.login, name="login"),
+    path("signup-endpoint", views.signup, name="signup")
 ]
--- main/views.py
+++ main/views.py
@@ -4,16 +4,59 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import UserDetails
 
+import re
+
 def index(request):
+    return render(request, "main/index.html")
+
+def signup(request):
     if request.method =="POST":
         email = request.POST.get("email")
         password = request.POST.get("password")
+        name = request.POST.get("name")
+        regex = r"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,7}"
+        try:
+            if re.fullmatch(regex, email) == None:
+                return render(request, "main/index.html", {"error":"You entered an invalid email address."})
+        except:
+            return render(request, "main/index.html", {"error":"You entered an invalid email address."})
+        if password == "":
+            return render(request, "main/index.html", {"error":"You must enter a password."})
+        if name == "":
+            return render(request, "main/index.html", {"error":"You must enter a name."})
 
         UserDetails.objects.create(
             email=email,
-            password=password
+            password=password,
+            name=name
         )
 
-        return redirect("index") #Reload page after saving to database
+        return redirect("signup") #Reload page after saving to database
+    
+    return render(request, "main/index.html")
+
+def login(request):
+    if request.method =="POST":
+        emailGiven = request.POST.get("email")
+        password = request.POST.get("password")
+        regex = r"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,7}"
+        try:
+            if re.fullmatch(regex, email) == None:
+                return render(request, "main/index.html", {"error":"You entered an invalid email address."})
+        except:
+            return render(request, "main/index.html", {"error":"You entered an invalid email address."})
+        if password == "":
+            return render(request, "main/index.html", {"error":"You must enter a password."})
+        
+        try:
+            UserDetails.objects.get(email=emailGiven)
+            return render(request, "main/index.html", {"error":"UserDetails.objects.get(email=email)"})
+        except emailGiven.DoesNotExist:
+            raise RuntimeError("Email does not exist")
+
+        #if db_email == email:
+
+
+        return redirect("login") #Reload page after saving to database
     
     return render(request, "main/index.html")
