4a28fced9860669e949ff21a99c31b15805eb968
hol0055 <230990461+hol0055@users.noreply.github.com>
2026-07-18 16:14:28 +1000

    Graph, RawDataDisplay and Insights Integration
    Turned dynamic graph integration into a function, allowing for easy defining of graphs, and ultimately less repeated code.
    
    Added more graphs onto page to follow mock-up design.
    
    Began work on Raw Data Display, Insights and Statistics.

--- main/views.py
+++ main/views.py
@@ -85,19 +85,33 @@ def dashboard(request): #BIG WIP -- NEED GRAPHS ACTUALLY WORKING!!
     
     if request.method =="GET":
         Sensor_last5 = SensorDetails.objects.order_by("-date_time")[:5] #Get last 5 entries in the SensorDetails table
-        values = []
+        elecLoadValues = []
+        waterValues = []
+        gasValues = []
+        batteryValues = []
+        solarValues = []
         times = []
         for i in Sensor_last5:
-            values.append(i.electricityload_value) #Add each entry's electricityload_value to the list
+            elecLoadValues.append(i.electricityload_value) #Add each entry's electricityload_value to the list
+            waterValues.append(i.water_usage) #Add each entry's water_usage to the list
+            gasValues.append(i.gas_usage) #Add each entry's gas_usage to the list
+            batteryValues.append(i.battery_charge) #Add each entry's battery_charge to the list
+            solarValues.append(i.solar_output) #Add each entry's solar_output to the list
             times.append(f"{str(i.date_time.strftime("%H:%M"))}") #Add the time of each entry to the list
         #Have to pass the code like this unfortunately, as im attempting to pass json as text in python...
-        #Essentially, this line defines the graph, its data set, labels, and other qualities
-        electricityload_graph_api_string = "const ctx = document.getElementById('electricityload_graph');new Chart(ctx, {type: 'line',data: {labels: "+f"{times}"+", datasets: [{label: 'Load (W)',data: "+f"{values}"+",borderWidth: 1}]},options: {scales: {y: {beginAtZero: false}}}});"
+        #Essentially, this line calls the makeGraph function, defining the graph, its data set, labels, and other qualities
+        battVsSolar_graph_api_string = makeMixedGraph("battVsSolar_graph", "Battery Charge VS Solar Output", "Time", "Output (W)", "Battery Output (W)", "Solar Output (W)", times, batteryValues, solarValues)
+        electricityload_graph_api_string = makeGraph("electricityload_graph", "Electricity Load", "Time", "Load (W)", elecLoadValues, times)
+        waterUsage_graph_api_string = makeGraph("waterUsage_graph", "Water Usage", "Time", "Water Used (L)", waterValues, times)
+        gasUsage_graph_api_string = makeGraph("gasUsage_graph", "Gas Usage", "Time", "Gas Used (L)", gasValues, times)
         #I then render the graph by passing it through to the frontend under the variable electricityload_graph_api, allowing me to directly render the code onto the page!
-        return render(request, "main/dashboard.html", {"electricityload_graph_api": mark_safe(electricityload_graph_api_string)}) #mark_safe used to treat the string as trusted HTML (stops auto-escaping by Django)
+        return render(request, "main/dashboard.html", {
+            "battVsSolar_graph_api": mark_safe(battVsSolar_graph_api_string),
+            "electricityload_graph_api": mark_safe(electricityload_graph_api_string),
+            "waterUsage_graph_api": mark_safe(waterUsage_graph_api_string),
+            "gasUsage_graph_api": mark_safe(gasUsage_graph_api_string)
+            }) #mark_safe used to treat the string as trusted HTML (stops auto-escaping by Django)
     return render(request, "main/dashboard.html")
-
-
 
 """
 ATTEMPT 1
@@ -122,3 +136,132 @@ if request.method =="POST":
             sent_string = f'{value_string},'.join(sent_string) #Append value_string onto the end of sent_string
         return render(request, "main/dashboard.html", {"electricityload_values": sent_string}) #mark_safe used to treat the string as trusted HTML (stops auto-escaping by Django)
 """
+
+def makeGraph(graph, title, labelx, labely, valuesx, valuesy):
+    return f'''
+const {graph}_ctx = document.getElementById('{graph}');
+new Chart({graph}_ctx, {{
+    type: 'line',
+    data: {{
+    labels: {valuesx},
+    datasets: [{{
+        label: '{labely}',
+        data: {valuesy},
+        borderWidth: 1
+    }}]
+}},
+options: {{
+    responsive: true,
+    maintainAspectRatio: false,
+    plugins: {{
+        title: {{
+            display: true,
+            text: '{title}',
+            color: '#000000',
+            font: {{
+                size: 20,
+                weight: 'bold',
+                family: 'Helvetica'
+            }},
+            padding: {{
+                top: 10,
+                bottom: 30
+            }}
+        }}
+    }},
+    scales: {{
+        x: {{
+            title: {{
+                display: true,
+                text: '{labelx}',
+                color: '#000000',
+                font: {{
+                    size: 14,
+                    weight: 'normal'
+                }}
+            }}
+        }},
+        y: {{
+            beginAtZero: false,
+            title: {{
+                display: true,
+                text: '{labely}',
+                color: '#000000',
+                font: {{
+                    size: 14,
+                    weight: 'normal'
+                }}
+            }}
+        }}
+    }}
+}}
+}});
+'''
+'''{graph}_ctx.style.width = '450';
+{graph}_ctx.style.height = '450';'''
+def makeMixedGraph(graph, title, labelx, labely, labely1, labely2, valuesx, valuesy1, valuesy2):
+    return f'''
+const ctx = document.getElementById('{graph}');
+new Chart(ctx, {{
+    type: 'line',
+    data: {{
+    labels: {valuesx},
+    datasets: [{{
+        label: '{labely1}',
+        data: {valuesy1},
+        borderWidth: 1
+    }},
+    {{
+        label: '{labely2}',
+        data: {valuesy2},
+        borderWidth: 1
+    }}
+    ]
+}},
+options: {{
+    responsive: true,
+    maintainAspectRatio: false,
+    plugins: {{
+        title: {{
+            display: true,
+            text: '{title}',
+            color: '#000000',
+            font: {{
+                size: 20,
+                weight: 'bold',
+                family: 'Helvetica'
+            }},
+            padding: {{
+                top: 10,
+                bottom: 30
+            }}
+        }}
+    }},
+    scales: {{
+        x: {{
+            title: {{
+                display: true,
+                text: '{labelx}',
+                color: '#000000',
+                font: {{
+                    size: 14,
+                    weight: 'normal'
+                }}
+            }}
+        }},
+        y: {{
+            beginAtZero: false,
+            title: {{
+                display: true,
+                text: '{labely}',
+                color: '#000000',
+                font: {{
+                    size: 14,
+                    weight: 'normal'
+                }}
+            }}
+        }}
+    }}
+}}
+}});
+'''
